b0f8313311acebcf8ad988cbb024cb4eb7a70df7
softwaretestlab <softwaretestlab@hotmail.com>
2022-09-14 14:55:02 +1000

    table

--- src/test/java/SeleniumLearning/AT01_NavigateTitleAssertScreenshot.java
+++ src/test/java/SeleniumLearning/AT01_NavigateTitleAssertScreenshot.java
@@ -1,5 +1,6 @@
 package SeleniumLearning;
 
+import BaseClass.OpenBrowser;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
@@ -11,28 +12,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
 public class AT01_NavigateTitleAssertScreenshot {
-
     @Test
-    public void NavigateGG() throws IOException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.harveynorman.com.au/");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        String ExpectedTitle = "Computers, Electrical, Furniture & Bedding | Harvey Norman Australia";
-        String ActualTitle = driver.getTitle();
+    public void navigate() throws IOException {
+//        //chromedriver setup
+//        WebDriverManager.chromedriver().setup();
+//      //  driver initiated ---meaning chrome browser will open
+//        WebDriver driver = new ChromeDriver();
+//        //window maximze
+//        driver.manage().window().maximize();
+//        //navigate to url
+//        driver.get("https://www.harveynorman.com.au/");
+        OpenBrowser.NavtoWebsite("https://www.harveynorman.com.au/");
+        //implicit wait
+        OpenBrowser.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //get title
+        String ActualTitle = OpenBrowser.driver.getTitle();
         System.out.println("ActualTitle : "+ActualTitle);
-        //Assertion Version-1 [here we are trying to match expected to actual]
-       // Assert.assertEquals(ExpectedTitle,ActualTitle);
-        //Assertion Version-2 [here we are checking for a specific value from entire title to match]
-      //  Assert.assertTrue(ActualTitle.contains("Furniture"));
+        String ExpectedTitle = "Computers, Electrical, Furniture & Bedding | Harvey Norman Australia";
+        //Assertion version-1
+        Assert.assertEquals(ExpectedTitle,ActualTitle);
+        //Assertion version-2
+        Assert.assertTrue(ActualTitle.contains("Electrical"));
         //Screenshot
-        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshotFile, new File("C:\\Users\\softw\\screenshot.png"));
+        File screenshotFile = ((TakesScreenshot)OpenBrowser.driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotFile, new File("C:\\Users\\softw\\screenshot.jpeg"));
 
 
     }
